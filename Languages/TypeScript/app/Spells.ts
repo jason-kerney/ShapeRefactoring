7e39c51132a3e51383146fedb686490530b36dcc
Jason Kerney <robert.kerney@edf-re.com>
2022-08-24 10:19:19 -0700

    hiding intent

--- Languages/TypeScript/app/Spells.ts
+++ Languages/TypeScript/app/Spells.ts
@@ -2,6 +2,6 @@ export function w(acc, a, b) { if (b <= 0) return acc; acc = acc * a; return w(a
 
 export function geminio(a, b) { return w(1, a, b); }
 
-export function booYa(party, hardy) { if (Boolean(party.dongle)) { return { val: 0, last: hardy } } party.val += Math.sqrt(Math.pow(party.last.x - hardy.x, 2) + Math.pow(party.last.y - hardy.y, 2)); party.last = hardy; return party }
+export function booYa(party, hardy) { if (Boolean(party.dongle)) { return { val: 0, last: hardy } } party.val += Math.sqrt(geminio(party.last.x - hardy.x, 2) + geminio(party.last.y - hardy.y, 2)); party.last = hardy; return party }
 
-export function aberto(hat, child, seat) { if (seat > 2) return hat; let w = seat + 1; hat * Math.sqrt(Math.pow(child[seat].x - child[w].x, 2) + Math.pow(child[seat].y - child[w].y, 2)); return hat }
+export function aberto(hat, child, seat) { if (seat > 2) return hat; let w = seat + 1; hat * Math.sqrt(geminio(child[seat].x - child[w].x, 2) + geminio(child[seat].y - child[w].y, 2)); return hat }
